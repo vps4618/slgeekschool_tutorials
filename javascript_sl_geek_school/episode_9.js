474d4e8dd11b24a1cd24c041f5e9b466f2c6e380
B.D.Vishwa Praveen Sarathchandra <88998540+VishwaPraveen4618@users.noreply.github.com>
2021-09-23 11:55:16 +0530

    Add files via upload

---
+++ javascript_sl_geek_school/episode_9.js
@@ -0,0 +1,25 @@
+// var x=5;
+// var sum=x+8;
+// console.log(sum);
+//select items you want to comment and press ctrl+/
+
+// let x=12;
+// let y=15;
+// let sum=x+y;
+// console.log(sum)
+
+// let x=2+3;
+// console.log(x);
+
+//how to make good variables
+
+// example 1 = distanceFromHomeToSchool
+
+// let x=5-6;
+// console.log(x);
+
+// x=5*7;
+// console.log(x);
+
+// let y=6/2;
+// console.log(y);
